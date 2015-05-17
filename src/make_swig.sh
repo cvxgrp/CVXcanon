81604e14e8de97c09bbeec1c36cacdb5f41dfe0f
Paul Quigley <piq93@stanford.edu>
2015-05-16 21:52:02 -0700

    changed gdb flags, so that we can debug

--- src/make_swig.sh
+++ src/make_swig.sh
@@ -7,4 +7,4 @@ swig -c++ -python CVXcanonPy.i
 # g++-4.9 -std=c++11 -Wall `python-config --include` -L/Users/jack/anaconda/lib -shared CVXcanon.cpp CVXcanonPy_wrap.cxx LinOp.cpp -o _CVXcanonPy.so
 # g++-4.9 -std=c++11  -I/Users/jack/anaconda/
 # g++-4.9 -std=c++11 -I/Users/jack/anaconda/include/python2.7 -L/Users/jack/anaconda/lib/python2.7 -o _CVXcanonPy.so -shared -fPIC CVXcanon.cpp LinOp.cpp  CVXcanonPy_wrap.cxx 
- g++-4.9 -std=c++11 -fPIC -Wall `python-config --include --ldflags` -shared LinOpOperations.cpp CVXcanon.cpp CVXcanonPy_wrap.cxx  -o _CVXcanonPy.so
+ g++-4.9 -std=c++11 -g -gdwarf-3 -fPIC -Wall `python-config --include --ldflags` -shared LinOpOperations.cpp CVXcanon.cpp CVXcanonPy_wrap.cxx  -o _CVXcanonPy.so
