fbc8f7841bf1321928f273a583cd8f3e3e5cb8b8
John Patterson Miller <millerjp@stanford.edu>
2015-05-19 00:42:37 -0700

    Smaller problem

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,24 +1,22 @@
 from cvxpy import *
 import numpy as np
 
-x = Variable(2)
-x = Variable(2)
-y = Variable(2)
-print Problem(Minimize(0), [x + y <= [1, 0]]).solve()
-
-'''
-x = Variable(2)
+# x = Variable(2)
+# x = Variable(2)
+# y = Variable(2)
+# print Problem(Minimize(0), [x + y <= [1, 0]]).solve()
 
 
+x = Variable(2)
 constraints = [ 2 * x[0] + x[1] >= 1, x[0] + 3 * x[1] >= 1, \
 x[0] >= 0, x[1] >= 0 ]
 
 objectives  =   [x[0] + x[1], -x[0] - x[1], x[0],\
 max_elemwise( x[0], x[1] ), x[0] ** 2 + 9 * x[1] ** 2 ]    
 
-for n, obj in enumerate(objectives):
+for n, obj in enumerate(objectives[3]):
 	prob = Problem(Minimize(obj), constraints)
 	prob.solve()
 	print "Solution to objective " + str(n)
 	print x.value
-	print obj.value '''
+	print obj.value
