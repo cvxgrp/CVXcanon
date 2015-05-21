1ff8f5a813bff34738ace01459bdb254aac0eabb
Jack Zhu <jackzhu@stanford.edu>
2015-05-20 23:18:11 -0700

    Writing test script to time all 364A problems

--- tests/python/364A_scripts/spacecraft_landing_data.py
+++ tests/python/364A_scripts/spacecraft_landing_data.py
@@ -63,18 +63,18 @@ p = ps[-2]# Last p which was feasible
 # use the following code to plot your trajectories
 # and the glide cone (don't modify)
 # -------------------------------------------------------
-fig = pass #plt.figure()
-ax = fig.gca(projection='3d')
+# fig = pass #plt.figure()
+# ax = fig.gca(projection='3d')
 
 X = np.linspace(-40, 55, num=30)
 Y = np.linspace(0, 55, num=30)
 X, Y = np.meshgrid(X, Y)
 Z = alpha*np.sqrt(X**2+Y**2)
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0)
+# ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=0)
 #Have your solution be stored in p
 
-ax.plot(xs=p.value[0,:].A1,ys=p.value[1,:].A1,zs=p.value[2,:].A1)
-ax.set_xlabel('x'); ax.set_ylabel('y'); ax.set_zlabel('z')
+# ax.plot(xs=p.value[0,:].A1,ys=p.value[1,:].A1,zs=p.value[2,:].A1)
+# ax.set_xlabel('x'); ax.set_ylabel('y'); ax.set_zlabel('z')
 
 
 pass #plt.title('Minimum time path ')
