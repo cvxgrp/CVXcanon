eeaf71acae2947d72d37f4cf69c61f4d27328300
Jack Zhu <jackzhu@stanford.edu>
2015-06-04 13:49:39 -0700

    Fixed setup files for pip

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,7 +2,8 @@ import numpy
 from setuptools import setup
 from distutils.core import Extension
 
-canon = Extension('_CVXcanon',['CVXcanon.i','../CVXcanon.cpp','../LinOpOperations.cpp'],
+canon = Extension('_CVXcanon',
+	sources=['CVXcanon.i','../CVXcanon.cpp','../LinOpOperations.cpp'],
 	swig_opts=['-c++','-I../','-outcurrentdir'],
 	include_dirs=[numpy.get_include(),'../'],
 	extra_compile_args=['-std=c++11'],
@@ -12,5 +13,5 @@ setup(
 	name='CVXcanon',
 	version='0.1',
 	ext_modules=[canon],
-    py_modules=['canonInterface']
+    py_modules=['canonInterface','CVXcanon']
 )
