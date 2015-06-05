888ac68ded1492d78fca6da6fdb6131a1169800e
Jack Zhu <jackzhu@stanford.edu>
2015-06-04 20:45:51 -0700

    Changed setup.py, removed bundle flag

--- src/python/setup.py
+++ src/python/setup.py
@@ -7,7 +7,7 @@ canon = Extension('_CVXcanon',
 	swig_opts=['-c++','-I../','-outcurrentdir'],
 	include_dirs=[numpy.get_include(),'../'],
 	extra_compile_args=['-std=c++11'],
-	extra_link_args=['-bundle']);
+	extra_link_args=[]);
 
 setup(
 	name='CVXcanon',
