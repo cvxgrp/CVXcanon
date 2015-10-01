6c6e3e887a913bc27a4c66c1b4f0624c8f116924
jpmpentwater <millerjp@stanford.edu>
2015-09-30 23:46:21 -0700

    Remove SWIG dependency!

--- setup.py
+++ setup.py
@@ -5,42 +5,14 @@ import numpy
 
 canon = Extension(
     'CVXcanon._CVXcanon',
-    sources=['CVXcanon/CVXcanon.i', 'src/CVXcanon.cpp', 'src/LinOpOperations.cpp'],
-    swig_opts=['-c++', '-outdir', 'CVXcanon', '-I./src/', '-I./CVXcanon/'],
+    sources=['src/CVXcanon.cpp', 'src/LinOpOperations.cpp', 'CVXcanon/CVXcanon_wrap.cpp'],
     include_dirs=['src/', 'CVXcanon/', 'include/Eigen', numpy.get_include()]
 )
 
-
-# Ensure the swig generated CVXcanon.py module is present before python
-# copies sources to the build directory
-class CustomBuild(build):
-    def run(self):
-        self.run_command('build_ext')
-        build.run(self)
-
-
-# Ensure the swig generated CVXcanon.py module is present before python
-# copies sources to the build directory
-class CustomInstall(install):
-    def run(self):
-        self.run_command('build_ext')
-        self.do_egg_install()
-
-
 setup(
     name='CVXcanon',
-    version='0.0.19',
-    # use_scm_version={
-    #     'version_scheme': 'guess-next-dev',
-    #     'local_scheme': 'dirty-tag',
-    #     'write_to': 'CVXcanon/_version.py'
-    # },
-
-    setup_requires=[
-        'setuptools>=18.0',
-        'setuptools-scm>1.5.4',
-        'numpy'
-    ],
+    version='0.0.20.1',
+    setup_requires=['numpy'],
     author='Jack Zhu, John Miller, Paul Quigley',
     author_email='jackzhu@stanford.edu, millerjp@stanford.edu, piq93@stanford.edu',
     ext_modules=[canon],
@@ -49,7 +21,6 @@ setup(
     description='A low-level library to perform the matrix building step in cvxpy, a convex optimization modeling software.',
     license='GPLv3',
     url='https://github.com/jacklzhu/CVXcanon',
-    cmdclass={'build': CustomBuild, 'install': CustomInstall},
     install_requires=[
         'numpy',
         'scipy',
