0af30bbb97cb5c8533a7afd8e6aa7678bde653f1
jpmpentwater <millerjp@stanford.edu>
2015-10-30 22:20:04 -0700

    Refactored directory structure
    Put the python specific code under src/python to make it easier to
    extend the library to R and Julia

--- setup.py
+++ setup.py
@@ -4,20 +4,20 @@ from distutils.command.build import build
 import numpy
 
 canon = Extension(
-    'CVXcanon._CVXcanon',
-    sources=['src/CVXcanon.cpp', 'src/LinOpOperations.cpp', 'CVXcanon/CVXcanon_wrap.cpp'],
-    include_dirs=['src/', 'CVXcanon/', 'include/Eigen', numpy.get_include()]
+    '_CVXcanon',
+    sources=['src/CVXcanon.cpp', 'src/LinOpOperations.cpp', 'src/python/CVXcanon_wrap.cpp'],
+    include_dirs=['src/', 'src/python/', 'include/Eigen', numpy.get_include()]
 )
 
 setup(
     name='CVXcanon',
-    version='0.0.22',
+    version='0.0.23',
     setup_requires=['numpy'],
     author='Jack Zhu, John Miller, Paul Quigley',
     author_email='jackzhu@stanford.edu, millerjp@stanford.edu, piq93@stanford.edu',
     ext_modules=[canon],
-    packages=find_packages(),
-    py_modules=['canonInterface'],
+    package_dir={'': 'src/python'},
+    py_modules=['canonInterface', 'CVXcanon'],
     description='A low-level library to perform the matrix building step in cvxpy, a convex optimization modeling software.',
     license='GPLv3',
     url='https://github.com/jacklzhu/CVXcanon',
