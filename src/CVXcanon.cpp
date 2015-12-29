7691b57d7633c630ac79066e16abe7d6e9eefddb
jpmpentwater <millerjp@stanford.edu>
2015-12-29 01:30:20 -0500

    Stubbed out more code

--- src/CVXcanon.cpp
+++ src/CVXcanon.cpp
@@ -84,24 +84,78 @@ std::map<OperatorType, std::vector<int> > compute_dimensions(std::map<OperatorTy
 	return dims;
 }
 
-pwork* initialize_problem(std::map<OperatorType, std::vector<LinOp *> > constr_map,
-													std::map<OperatorType, std::vector<int> > dims_map){
-
-
+// returns a vector of all variable 
+std::vector<std::vector<int> > get_expr_vars(LinOp * expr){
+	// TODO!
+	return NULL;
 }
 
+std::map<int, int> get_var_offsets(LinOp * objective, std::vector<LinOp*> constraints){
+	// TODO!
+	return NULL;
+}
 
+std::vector<LinOp *> concatenate(std::vector<LinOp *> A, std::vector<LinOp *> B, std::vector<LinOp *> C){
+	std::vector<LinOp *> ABC;
+	ABC.reserve(A.size() + B.size() + C.size());
+	ABC.insert(ABC.end(), A.begin(), A.end());
+	ABC.insert(ABC.end(), B.begin(), B.end());
+	ABC.insert(ABC.end(), C.begin(), C.end());
+	return ABC;
+}
 
+pwork* initialize_problem(Sense sense, LinOp * objective, 
+												 	std::map<OperatorType, std::vector<LinOp *> > constr_map,
+													std::map<OperatorType, std::vector<int> > dims_map,
+													std::map<int, int> var_offsets){
+	/* get problem data */
+	std::vector<LinOp *> objVec;
+	objVec.push_back(objective);
+	std::vector<LinOp *> ineqConstr = concatenate(constr_map[LEQ], constr_map[SOC], constr_map[EXP]);
 
+	ProblemData objData = build_matrix(objVec, var_offsets);
+	ProblemData eqData = build_matrix(constr_map[EQ], var_offsets);
+	ProblemData ineqData = build_matrix(ineqConstr, var_offsets);
+
+	/* Problem Dimensions */
+	// TODO!!! 
+	idxint n 	= 1; //Number of primal variables
+	idxint m 	= 2; //Number of constraints, i.e. dimension 1 of the matrix G and length of vector h
+	idxint p 	= 0; //Number of equality constraints, i.e. dimension 1 of matrix A and length of vector b
+	idxint l 	= 2; //Dimension of the positive orthant Rl+
+	idxint ncones = 0; //Number of second-order cones present in problem
+	idxint *q = NULL;	 //[1] = {3};		 //Array of length ncones; q[i] defines the dimension of the cone i
+	idxint e 	= 0; //Number of exponential cones present in problem
+	
+	/* Extract G and A matrices in CCS */
+	// TODO!!!
+	pfloat Gpr[] = {-1.000000000000000000e+00, 1.000000000000000000e+00};
+	idxint Gjc[] = {0.0, 2}; //indptr
+	idxint Gir[] = {0.0, 1};   //indices
+	pfloat *Apr = NULL;//[0];
+	idxint *Ajc = NULL;//[0];
+	idxint *Air = NULL;//[0];
+	pfloat c[] = {-1}; //Array of length n
+	pfloat h[] = {-2.000000000000000000e+00, 1.000000000000000000e+00}; //Array of length m
+	pfloat *b = NULL;			//Array of length p. Can be NULL if no equality constraints are present
+
+	// TODO: CHANGE C depending on the sense!
+	pwork* problem = ECOS_setup(n, m, p, l, ncones, q, e,
+													 		Gpr, Gjc, Gir,
+													 		Apr, Ajc, Air,
+													 		c, h, b);
+	return problem;
+}
 
 Solution solve(Sense sense, LinOp* objective, std::vector<LinOp *> constraints,
 							 std::map<std::string, double> arguments){
 	/* Pre-process constraints */
 	std::map<OperatorType, std::vector<LinOp *> > constr_map = filter_constraints(contraints);
 	std::map<OperatorType, std::vector<int> > dims_map = compute_dimensions(constr_map);
+	std::map<int, int> var_offsets = get_var_offsets(LinOp* objective, constraints);
 
 	/* Instiantiate problem data (convert appropriate linOp trees to sparse matrix form) */
-	pwork* problem = initialize_problem(constr_map, dims_map);
+	pwork* problem = initialize_problem(sense, objective, constr_map, dims_map, var_offsets);
 
 	/* Call ECOS and solve the problem */
 	idxint status = ECOS_solve(problem);
