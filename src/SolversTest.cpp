5967f9e378d110ddfa81e2741d0bb91bd9f13a3e
Jack Zhu <jackzhu@stanford.edu>
2015-12-30 16:07:49 +0800

    Adding a test of new CVXcanon API

--- src/SolversTest.cpp
+++ src/SolversTest.cpp
@@ -4,14 +4,14 @@
 #import <ecos.h>
 #import <stdio.h>
 #import "ProblemData.hpp"
+#import "Solution.hpp"
+#import "CVXcanon.hpp"
 
 
 // Just to show that ecos compiles with CVXcanon
 
-int main()
+void testInfeasible()
 {
-	ProblemData problemdata; // lol
-
 	idxint n 		= 1; //Number of primal variables
 	idxint m 		= 2; //Number of constraints, i.e. dimension 1 of the matrix G and length of vector h
 	idxint p 		= 0; //Number of equality constraints, i.e. dimension 1 of matrix A and length of vector b
@@ -41,7 +41,90 @@ int main()
 	ECOS_cleanup(work, 0);
 
 	printf("ECOS finished with %ld\n", flag);
+}
 
+
+/*
+
+LinOp(type='variable', size=(1, 1), args=[], data=0)
+
+[LinLeqConstr(expr=
+	LinOp(type='sum', size=(1, 1), args=[
+		LinOp(type='scalar_const', size=(1, 1), args=[], data=1), 
+		LinOp(type='neg', size=(1, 1), args=[
+			LinOp(type='variable', size=(1, 1), args=[], data=0)]
+		, data=None)]
+	, data=None)
+, constr_id=2, size=(1, 1))]
+
+*/
+
+void testCVXCanon()
+{
+	LinOp objective;
+	objective.type = VARIABLE;
+	objective.size.push_back(1);
+	objective.size.push_back(1);
+	objective.sparse = false;
+	objective.dense_data = Eigen::MatrixXd(1,1);
+	objective.dense_data(0,0) = 0;
+
+	// Constraint root
+	LinOp constraint;
+	constraint.type = LEQ;
+	constraint.size.push_back(1);
+	constraint.size.push_back(1);
+	constraint.sparse = false;
+	constraint.dense_data = Eigen::MatrixXd(1,1);
+	constraint.dense_data(0,0) = 2;
+
+	// Constant (1)
+	LinOp constant;
+	constant.type = SUM;
+	constant.size.push_back(1);
+	constant.size.push_back(1);
+	constant.sparse = false;
+
+	// Variable (x)
+	LinOp x;
+	x.type = VARIABLE;
+	x.size.push_back(1);
+	x.size.push_back(1);
+	x.sparse = false;
+	x.dense_data = Eigen::MatrixXd(1,1);
+	x.dense_data(0,0) = 0;
+
+	// Negative
+	LinOp neg;
+	neg.type = NEG;
+	neg.size.push_back(1);
+	neg.size.push_back(1);
+	neg.sparse = false;
+	neg.args.push_back(&x);
+
+	// Sum
+	LinOp sum;
+	sum.type = SUM;
+	sum.size.push_back(1);
+	sum.size.push_back(1);
+	sum.sparse = false;
+
+	sum.args.push_back(&constant);
+	sum.args.push_back(&neg);
+
+	constraint.args.push_back(&sum);
+
+	std::vector< LinOp* > constraints;
+	constraints.push_back(&constraint);
+
+	std::map<std::string, double> arguments;
+	Solution s = solve(MINIMIZE, &objective, constraints, arguments);
+}
+
+int main()
+{
+	testInfeasible();
+	testCVXCanon();
 }
 
 #endif
